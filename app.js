3f7949484b57f9c358c6eae75dbe115488eaf299
shaneocon <shane.pat.oconnell@gmail.com>
2020-12-17 08:14:44 -0800

    time clock

--- app.js
+++ app.js
@@ -1,17 +1,37 @@
-    //------ "Global" function------//
-$(function() {
-    //------- Adds current Day to header" -------//
-    var currentDayEl = $("#currentDay");
-    currentDayEl.text(moment().format("MMM Do YY"));
-
-
-
-
-
-
-
-
-
-
-
+$(function () {
+    //Adds current day to currentDay Head
+    var currentdayEl = $("#currentDay");
+    currentdayEl.text(moment().format("MMMM Do YYYY, h:mm a"));
+    var currentHourTime = parseInt(moment().format('k'));
+    //Change color
+    for (var hourCount = 9; hourCount < 18; hourCount++) {
+        var hourgrabEl = $("#hour-" + hourCount);
+        if(hourCount < currentHourTime) {
+            hourgrabEl.attr("class","row time-block past");
+        }
+        else if(hourCount === currentHourTime) {
+            hourgrabEl.attr("class","row time-block present");
+        }
+        else {
+            hourgrabEl.attr("class","row time-block future");
+        }
+    }
+    // var descriptionArray = [{hour: ??, description: ?? }];
+    // $(".saveBtn").on("click", function() {
+    //     var tempString = $(this.previousElementSibling).val();
+    //     console.log(tempString);
+    //     var hourTemp = $(this.parentElement).i
+    //     localStorage.setItem("description", JSON.stringify(tempString));
+    // })
+    // function init() {
+    //     var storedHighscores = JSON.parse(localStorage.getItem("highscores"));
+    //     if (storedHighscores !== null) {
+    //         highscores = storedHighscores;
+    //     }
+    // }
+    function storeDescriptions() {
+    }
+    console.log(moment().format('k'));
+    console.log(currentHourTime);
+    console.log(typeof(currentHourTime));
 });
