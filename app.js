aa8ad016afc8f11dacb4eeeaf708e0cc20fd6cba
shaneocon <shane.pat.oconnell@gmail.com>
2020-12-16 19:47:26 -0800

    added currentDayEl variable, for header display

---
+++ app.js
@@ -0,0 +1,17 @@
+    //------ "Global" function------//
+$(function() {
+    //------- Adds current Day to header" -------//
+    var currentDayEl = $("#currentDay");
+    currentDayEl.text(moment().format("MMM Do YY"));
+
+
+
+
+
+
+
+
+
+
+
+});
